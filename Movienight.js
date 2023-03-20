e813d8c07bef69f873272bfb41680e61c39a57ca
MUKONGE ERIC <mukonge.eric@gmail.com>
2023-03-20 17:21:09 +0300

    First commit

---
+++ Movienight.js
@@ -0,0 +1,12 @@
+// MOVIE NIGHT
+var zipcode = 98104;
+var JoesFavoriteMovie = "Forbidden Planet";
+var movieTicket$ = 9;
+if (movieTicket$ >= 9) 
+{
+alert("Too much!");
+} 
+else 
+{
+alert("We're going to see " + JoesFavoriteMovie);
+}
